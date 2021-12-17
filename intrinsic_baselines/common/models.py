00973bc888a0b9861bd0d282831287285e0375a4
Federico Landi <federico.landi@unimore.it>
2021-12-17 17:09:14 +0100

    updated requirements.txt and imports

--- intrinsic_baselines/common/models.py
+++ intrinsic_baselines/common/models.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 
 class ForwardModel(nn.Module):
